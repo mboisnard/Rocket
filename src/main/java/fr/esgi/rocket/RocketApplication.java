fb7a46b5ceab6bb615519e69254fa85b8b8e5878
Valentin Fries <contact@fries.io>
2017-06-09 12:05:00 +0200

    Spring Boot setup

---
+++ src/main/java/fr/esgi/rocket/RocketApplication.java
@@ -0,0 +1,12 @@
+package fr.esgi.rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketApplication.class, args);
+	}
+}
